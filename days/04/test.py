e2e240fde2610f05bca7247ae48e8f81cf07514d
Martin Grignard <mar.grignard@gmail.com>
2025-12-08 15:14:49 +0100

    test: :test_tube: Add `test_parse_diagram`

--- days/04/test.py
+++ days/04/test.py
@@ -2,3 +2,23 @@
 
 https://adventofcode.com/2025/day/4
 """
+
+from io import StringIO
+from typing import TextIO
+
+import numpy as np
+import pytest
+
+from main import parse_diagram
+
+
+@pytest.mark.parametrize(
+    ("diagram", "expected_array"),
+    [
+        (StringIO("..\n.."), np.array([[False, False], [False, False]])),
+        (StringIO("@@\n@@"), np.array([[True, True], [True, True]])),
+        (StringIO("@.\n.@"), np.array([[True, False], [False, True]])),
+    ],
+)
+def test_parse_diagram(diagram: TextIO, expected_array: np.ndarray) -> None:
+    assert np.all(parse_diagram(diagram) == expected_array)
