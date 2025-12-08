0e60dbdc97f239cf4806c21e078f8c919aae2f77
Martin Grignard <mar.grignard@gmail.com>
2025-12-08 20:56:09 +0100

    Initialise day 6 :tada:

---
+++ days/06/main.py
@@ -0,0 +1,11 @@
+"""Day 6: Trash Compactor
+
+https://adventofcode.com/2025/day/6
+"""
+
+
+def main() -> None: ...
+
+
+if __name__ == "__main__":
+    main()
---
+++ days/06/test.py
@@ -0,0 +1,4 @@
+"""Day 6: Trash Compactor
+
+https://adventofcode.com/2025/day/6
+"""
