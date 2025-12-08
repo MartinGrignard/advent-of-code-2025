b83c314f428673d11c8a5cfd282d060474975ca3
Martin Grignard <mar.grignard@gmail.com>
2025-12-08 11:44:38 +0100

    feat: :sparkles: Add `get_invalid_ids` method to `Range` class

--- days/02/main.py
+++ days/02/main.py
@@ -4,7 +4,11 @@ https://adventofcode.com/2025/day/2
 """
 
 from dataclasses import dataclass
-from typing import Self
+import re
+from typing import Generator, Self
+
+
+REPEATING_NUMBER_PATTERN = re.compile(r"^(\d+)\1$")
 
 
 @dataclass
@@ -23,6 +27,12 @@ class Range:
     def from_string(cls: type[Self], string: str) -> Self:
         """Parse a range form a string."""
         return cls(*[int(part) for part in string.split("-")])
+
+    def get_invalid_ids(self: Self) -> Generator[int, None, None]:
+        """Extract the invalid product IDs of the range."""
+        for product_id in range(self.start, self.end + 1):
+            if REPEATING_NUMBER_PATTERN.match(str(product_id)) is not None:
+                yield product_id
 
 
 def main() -> None: ...
