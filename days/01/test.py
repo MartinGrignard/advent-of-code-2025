926916104bcb334ccf52feef569228344df99f2b
Martin Grignard <mar.grignard@gmail.com>
2025-12-08 10:03:33 +0100

    test: :test_tube: Add `test_parse_clicks`

--- days/01/test.py
+++ days/01/test.py
@@ -2,3 +2,20 @@
 
 https://adventofcode.com/2025/day/1
 """
+
+import pytest
+
+from main import parse_clicks
+
+
+@pytest.mark.parametrize(
+    ("string", "expected_clicks"),
+    [
+        ("L1", -1),
+        ("L10", -10),
+        ("R1", 1),
+        ("R10", 10),
+    ],
+)
+def test_parse_clicks(string: str, expected_clicks: int) -> None:
+    assert parse_clicks(string) == expected_clicks
