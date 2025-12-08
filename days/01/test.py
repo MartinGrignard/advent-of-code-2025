3be1eb317d2ba7fc9a1314a689e48b5a799030e6
Martin Grignard <mar.grignard@gmail.com>
2025-12-08 10:13:33 +0100

    test: :test_tube: Add `test_dial_rotate`

--- days/01/test.py
+++ days/01/test.py
@@ -5,7 +5,7 @@ https://adventofcode.com/2025/day/1
 
 import pytest
 
-from main import parse_clicks
+from main import Dial, parse_clicks
 
 
 @pytest.mark.parametrize(
@@ -19,3 +19,19 @@ from main import parse_clicks
 )
 def test_parse_clicks(string: str, expected_clicks: int) -> None:
     assert parse_clicks(string) == expected_clicks
+
+
+@pytest.mark.parametrize(
+    ("dial", "clicks", "expected_position"),
+    [
+        (Dial(2, 0), 0, 0),
+        (Dial(2, 0), 1, 1),
+        (Dial(2, 1), -1, 0),
+        (Dial(2, 0), -1, 1),
+        (Dial(2, 1), 1, 0),
+        (Dial(2, 0), 3, 1),
+    ],
+)
+def test_dial_rotate(dial: Dial, clicks: int, expected_position: int) -> None:
+    dial.rotate(clicks)
+    assert dial.position == expected_position
