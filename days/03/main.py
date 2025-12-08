c366539bd612c913e9c9fc8608cfdb52a84ad9de
Martin Grignard <mar.grignard@gmail.com>
2025-12-08 13:52:59 +0100

    feat: :sparkles: Add `Index` class

--- days/03/main.py
+++ days/03/main.py
@@ -3,7 +3,24 @@
 https://adventofcode.com/2025/day/3
 """
 
-from typing import Generator
+from typing import Generator, Iterable, Self
+
+
+class Index:
+    """An index of the joltages."""
+
+    def __init__(self: Self, joltages: Iterable[int]) -> None:
+        self._index = [list() for _ in range(9)]
+        for position, joltage in enumerate(joltages):
+            self._index[joltage - 1].append(position)
+
+    def to_dict(self: Self) -> dict[int, list[int]]:
+        """Convert the index into a dictionary."""
+        return {
+            value + 1: positions
+            for value, positions in enumerate(self._index)
+            if positions
+        }
 
 
 def parse_joltages(string: str) -> Generator[int, None, None]:
