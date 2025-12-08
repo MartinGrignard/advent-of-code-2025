d82bb568b29dee3ebc28aa516333f0d5854b87e3
Martin Grignard <mar.grignard@gmail.com>
2025-12-08 13:25:51 +0100

    test: :test_tube: Add `test_parse_joltages`

--- days/03/test.py
+++ days/03/test.py
@@ -2,3 +2,20 @@
 
 https://adventofcode.com/2025/day/3
 """
+
+from typing import Iterable
+
+import pytest
+
+from main import parse_joltages
+
+
+@pytest.mark.parametrize(
+    ("string", "expected_joltages"),
+    [
+        ("1", [1]),
+        ("12345", [1, 2, 3, 4, 5]),
+    ],
+)
+def test_parse_joltages(string: str, expected_joltages: Iterable[int]) -> None:
+    assert list(parse_joltages(string)) == list(expected_joltages)
