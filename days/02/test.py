0e46011d22ea89844c8b99f2c1c008ab48ac303f
Martin Grignard <mar.grignard@gmail.com>
2025-12-08 11:44:20 +0100

    test: :test_tube: Add `test_range_get_invalid_ids`

--- days/02/test.py
+++ days/02/test.py
@@ -3,6 +3,7 @@
 https://adventofcode.com/2025/day/2
 """
 
+from typing import Iterable
 import pytest
 
 from main import Range
@@ -17,3 +18,18 @@ from main import Range
 )
 def test_range_from_string(string: str, expected_ends: tuple[int, int]) -> None:
     assert Range.from_string(string).ends == expected_ends
+
+
+@pytest.mark.parametrize(
+    ("range", "expected_invalid_ids"),
+    [
+        (Range(11, 22), [11, 22]),
+        (Range(95, 115), [99]),
+        (Range(998, 1012), [1010]),
+        (Range(1188511880, 1188511890), [1188511885]),
+    ],
+)
+def test_range_get_invalid_ids(
+    range: Range, expected_invalid_ids: Iterable[int]
+) -> None:
+    assert list(range.get_invalid_ids()) == list(expected_invalid_ids)
