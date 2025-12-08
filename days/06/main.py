278073b944880e1e91529e1dca0178928f827bd2
Martin Grignard <mar.grignard@gmail.com>
2025-12-08 21:11:32 +0100

    feat: :sparkles: Add `parse_problems`

--- days/06/main.py
+++ days/06/main.py
@@ -3,6 +3,25 @@
 https://adventofcode.com/2025/day/6
 """
 
+import re
+from typing import Literal, TextIO, TypeAlias
+
+
+Problem: TypeAlias = tuple[Literal["+", "*"], tuple[int, ...]]
+
+
+REPLACE_SPACES_PATTERN = re.compile(r"\s+")
+
+
+def parse_problems(strings: TextIO) -> list[Problem]:
+    """Parse problems from a text."""
+    rows = [
+        re.sub(REPLACE_SPACES_PATTERN, " ", string).strip().split(" ")
+        for string in strings
+    ]
+    values = zip(*[[int(number) for number in row] for row in rows[:-1]])
+    return list(zip(rows[-1], values))
+
 
 def main() -> None: ...
 
