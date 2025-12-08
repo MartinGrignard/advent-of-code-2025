aeb544728064a4083aef3665da58b658afa36536
Martin Grignard <mar.grignard@gmail.com>
2025-12-08 15:15:04 +0100

    feat: :sparkles: Add `parse_diagram`

--- days/04/main.py
+++ days/04/main.py
@@ -3,6 +3,17 @@
 https://adventofcode.com/2025/day/4
 """
 
+from typing import TextIO
+
+import numpy as np
+
+
+def parse_diagram(diagram: TextIO) -> np.ndarray:
+    """Convert a diagram into an array."""
+    return np.array(
+        [[character == "@" for character in row.strip()] for row in diagram]
+    )
+
 
 def main() -> None: ...
 
