62e1c90722e73d20d7036dcefc31bf131acfed6d
Martin Grignard <mar.grignard@gmail.com>
2025-12-08 15:29:42 +0100

    feat: :sparkles: Add `count_accessible_scrolls`

--- days/04/main.py
+++ days/04/main.py
@@ -6,12 +6,27 @@ https://adventofcode.com/2025/day/4
 from typing import TextIO
 
 import numpy as np
+from scipy.signal import convolve2d
 
 
 def parse_diagram(diagram: TextIO) -> np.ndarray:
     """Convert a diagram into an array."""
     return np.array(
         [[character == "@" for character in row.strip()] for row in diagram]
+    )
+
+
+def count_accessible_scrolls(diagram: np.ndarray, threshold: int) -> np.ndarray:
+    """Count the scrolls with less than a specific amount of adjacent scrolls."""
+    kernel = np.array(
+        [
+            [1, 1, 1],
+            [1, 0, 1],
+            [1, 1, 1],
+        ]
+    )
+    return np.count_nonzero(
+        np.bitwise_and(convolve2d(diagram, kernel, mode="same") < threshold, diagram)
     )
 
 
