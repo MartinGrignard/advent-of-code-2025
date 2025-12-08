41e87b7bb1fa9beb6ef1b47fec702c306bc53987
Martin Grignard <mar.grignard@gmail.com>
2025-12-08 15:58:35 +0100

    test: :test_tube: Add `test_range_from_string`

--- days/05/test.py
+++ days/05/test.py
@@ -2,3 +2,18 @@
 
 https://adventofcode.com/2025/day/5
 """
+
+import pytest
+
+from main import Range
+
+
+@pytest.mark.parametrize(
+    ("string", "expected_range"),
+    [
+        ("3-5", (3, 5)),
+        ("10-14", (10, 14)),
+    ],
+)
+def test_range_from_string(string: str, expected_range: tuple[int, int]) -> None:
+    assert Range.from_string(string).bounds == expected_range
