d74c074cd0c4fd4e6cb1b11051e4251d8bcf124e
Martin Grignard <mar.grignard@gmail.com>
2025-12-08 21:11:11 +0100

    test: :test_tube: Add `test_parse_problems`

--- days/06/test.py
+++ days/06/test.py
@@ -2,3 +2,16 @@
 
 https://adventofcode.com/2025/day/6
 """
+
+from io import StringIO
+
+from main import parse_problems
+
+
+def test_parse_problems() -> None:
+    strings = StringIO("1 2\n345 678\n+ *")
+    expected_problems = [
+        ("+", (1, 345)),
+        ("*", (2, 678)),
+    ]
+    assert parse_problems(strings) == expected_problems
