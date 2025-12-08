93068c69b4bb50ee26f7bd25441f837ceb465735
Martin Grignard <mar.grignard@gmail.com>
2025-12-08 13:26:05 +0100

    feat: :sparkles: Add `parse_joltages`

--- days/03/main.py
+++ days/03/main.py
@@ -3,6 +3,13 @@
 https://adventofcode.com/2025/day/3
 """
 
+from typing import Generator
+
+
+def parse_joltages(string: str) -> Generator[int, None, None]:
+    """Convert a string into its integer joltages."""
+    return (int(joltage) for joltage in string)
+
 
 def main() -> None: ...
 
