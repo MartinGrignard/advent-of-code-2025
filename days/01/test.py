1558e49c602b242c43d5e33ac06d26534ea82209
Martin Grignard <mar.grignard@gmail.com>
2025-12-08 11:03:24 +0100

    test: :test_tube: Add `test_rotation_passes_by`

--- days/01/test.py
+++ days/01/test.py
@@ -6,7 +6,7 @@ https://adventofcode.com/2025/day/1
 from typing import Iterable
 import pytest
 
-from main import Dial, count_ends_on, count_passes_by, parse_clicks
+from main import Dial, count_ends_on, count_passes_by, parse_clicks, Rotation
 
 
 @pytest.mark.parametrize(
@@ -49,6 +49,25 @@ def test_dial_rotate(dial: Dial, clicks: int, expected_position: int) -> None:
 )
 def test_count_ends_on(instructions: Iterable[int], expected_count: int) -> None:
     assert count_ends_on(1, Dial(2, 0), instructions) == expected_count
+
+
+@pytest.mark.parametrize(
+    ("rotation", "position", "expected_answer"),
+    [
+        (Rotation(0, 2, 0), 1, False),
+        (Rotation(2, 2, 0), 1, False),
+        (Rotation(2, 3, 0), 1, True),
+        (Rotation(2, 3, 0), 2, False),
+        (Rotation(-2, 3, 2), 1, True),
+        (Rotation(-2, 3, 2), 0, False),
+        (Rotation(-2, 3, 0), 2, True),
+        (Rotation(-2, 3, 0), 1, False),
+        (Rotation(2, 3, 2), 0, True),
+        (Rotation(2, 3, 2), 1, False),
+    ]
+)
+def test_rotation_passes_by(rotation: Rotation, position: int, expected_answer: bool) -> None:
+    assert rotation.passes_by_in_last_turn(position) == expected_answer
 
 
 @pytest.mark.parametrize(
