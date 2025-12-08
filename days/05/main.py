8a088551ef8a6e2e3fc9f1bc7688bed5be9b85d9
Martin Grignard <mar.grignard@gmail.com>
2025-12-08 15:58:53 +0100

    feat: :sparkles: Add `Range` class

--- days/05/main.py
+++ days/05/main.py
@@ -3,6 +3,26 @@
 https://adventofcode.com/2025/day/5
 """
 
+from dataclasses import dataclass
+from typing import Self
+
+
+@dataclass
+class Range:
+    """A range of ingredient IDs."""
+
+    lower: int
+    upper: int
+
+    @property
+    def bounds(self: Self) -> tuple[int, int]:
+        """The bounds of the range."""
+        return self.lower, self.upper
+
+    @classmethod
+    def from_string(cls: type[Self], string: str) -> Self:
+        return cls(*[int(number) for number in string.split("-")])
+
 
 def main() -> None: ...
 
