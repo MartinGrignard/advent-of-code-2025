61a1cf0616b215de0dd1a53b1a38e5c387223a13
Martin Grignard <mar.grignard@gmail.com>
2025-12-08 10:05:15 +0100

    feat: :sparkles: Add `parse_clicks`

--- days/01/main.py
+++ days/01/main.py
@@ -4,6 +4,11 @@ https://adventofcode.com/2025/day/1
 """
 
 
+def parse_clicks(string: str) -> int:
+    """Parse a string describing a rotation."""
+    return (-1 + 2 * int(string[0] == "R")) * int(string[1:])
+
+
 def main() -> None: ...
 
 
