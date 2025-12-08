9b9ff01fe3d983ea301bf8075b572778d6894694
Martin Grignard <mar.grignard@gmail.com>
2025-12-08 11:36:01 +0100

    feat: :sparkles: Add `Range` class

--- days/02/main.py
+++ days/02/main.py
@@ -3,6 +3,27 @@
 https://adventofcode.com/2025/day/2
 """
 
+from dataclasses import dataclass
+from typing import Self
+
+
+@dataclass
+class Range:
+    """A product IDs range."""
+
+    start: int
+    end: int
+
+    @property
+    def ends(self: Self) -> tuple[int, int]:
+        """The ends of the range."""
+        return self.start, self.end
+
+    @classmethod
+    def from_string(cls: type[Self], string: str) -> Self:
+        """Parse a range form a string."""
+        return cls(*[int(part) for part in string.split("-")])
+
 
 def main() -> None: ...
 
