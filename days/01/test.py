6b7fc5e280b3495d0cc2d0695ce2e31b71366168
Martin Grignard <mar.grignard@gmail.com>
2025-12-08 10:33:06 +0100

    test: :test_tube: Add `test_count_passes_by`

--- days/01/test.py
+++ days/01/test.py
@@ -6,7 +6,7 @@ https://adventofcode.com/2025/day/1
 from typing import Iterable
 import pytest
 
-from main import Dial, count_ends_on, parse_clicks
+from main import Dial, count_ends_on, count_passes_by, parse_clicks
 
 
 @pytest.mark.parametrize(
@@ -49,3 +49,17 @@ def test_dial_rotate(dial: Dial, clicks: int, expected_position: int) -> None:
 )
 def test_count_ends_on(instructions: Iterable[int], expected_count: int) -> None:
     assert count_ends_on(1, Dial(2, 0), instructions) == expected_count
+
+
+@pytest.mark.parametrize(
+    ("instructions", "expected_count"),
+    [
+        ([2], 1),
+        ([1], 1),
+        ([1, 1], 1),
+        ([1, 2], 2),
+        ([1, 3], 2),
+    ],
+)
+def test_count_passes_by(instructions: Iterable[int], expected_count: int) -> None:
+    assert count_passes_by(1, Dial(2, 0), instructions)
