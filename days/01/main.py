b11805bc784d584f39240688e9470905b1a28077
Martin Grignard <mar.grignard@gmail.com>
2025-12-08 10:13:51 +0100

    feat: :sparkles: Add `Dial` class with `rotate` method

--- days/01/main.py
+++ days/01/main.py
@@ -3,6 +3,29 @@
 https://adventofcode.com/2025/day/1
 """
 
+from typing import Self
+
+
+class Dial:
+    """A circular dial."""
+
+    def __init__(self: Self, size: int, position: int) -> None:
+        self._size = size
+        self._position = position
+
+    @property
+    def size(self: Self) -> int:
+        return self._size
+
+    @property
+    def position(self: Self) -> int:
+        return self._position
+
+    def rotate(self: Self, clicks: int) -> None:
+        """Rotate the dial."""
+        rotation = clicks % self.size
+        self._position = (self.size + self._position + rotation) % self.size
+
 
 def parse_clicks(string: str) -> int:
     """Parse a string describing a rotation."""
