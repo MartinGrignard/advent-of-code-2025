a11a00809578cecc20c425fa9822bd6f50e40150
Martin Grignard <mar.grignard@gmail.com>
2025-12-08 11:33:09 +0100

    test: :test_tube: Add `test_range_from_string`

--- days/02/test.py
+++ days/02/test.py
@@ -2,3 +2,18 @@
 
 https://adventofcode.com/2025/day/2
 """
+
+import pytest
+
+from main import Range
+
+
+@pytest.mark.parametrize(
+    ("string", "expected_ends"),
+    [
+        ("1-10", (1, 10)),
+        ("10-100", (10, 100)),
+    ],
+)
+def test_range_from_string(string: str, expected_ends: tuple[int, int]) -> None:
+    assert Range.from_string(string).ends == expected_ends
