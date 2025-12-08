2241f87906b7c1e110728ece8e240184bad5480c
Martin Grignard <mar.grignard@gmail.com>
2025-12-08 14:52:49 +0100

    test: :test_tube: Update `test_get_bank_joltage` to account for number of cells

--- days/03/test.py
+++ days/03/test.py
@@ -22,14 +22,20 @@ def test_parse_joltages(string: str, expected_joltages: Iterable[int]) -> None:
 
 
 @pytest.mark.parametrize(
-    ("bank", "expected_joltage"),
+    ("bank", "size", "expected_joltage"),
     [
-        (parse_joltages("987654321111111"), 98),
-        (parse_joltages("811111111111119"), 89),
-        (parse_joltages("234234234234278"), 78),
-        (parse_joltages("818181911112111"), 92),
-        (parse_joltages("818981911112111"), 99),
+        (parse_joltages("987654321111111"), 2, 98),
+        (parse_joltages("811111111111119"), 2, 89),
+        (parse_joltages("234234234234278"), 2, 78),
+        (parse_joltages("818181911112111"), 2, 92),
+        (parse_joltages("818981911112111"), 2, 99),
+        (parse_joltages("987654321111111"), 12, 987654321111),
+        (parse_joltages("811111111111119"), 12, 811111111119),
+        (parse_joltages("234234234234278"), 12, 434234234278),
+        (parse_joltages("818181911112111"), 12, 888911112111),
     ],
 )
-def test_get_bank_joltage(bank: Iterable[int], expected_joltage: int) -> None:
-    assert get_bank_joltage(bank) == expected_joltage
+def test_get_bank_joltage(
+    bank: Iterable[int], size: int, expected_joltage: int
+) -> None:
+    assert get_bank_joltage(bank, size) == expected_joltage
