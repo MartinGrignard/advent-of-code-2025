7805522bee5674808f7518938d41a0a50c1f9b50
Martin Grignard <mar.grignard@gmail.com>
2025-12-08 10:00:26 +0100

    Initialise day 1 :tada:

---
+++ days/01/main.py
@@ -0,0 +1,11 @@
+"""Day 1: Secret Entrance
+
+https://adventofcode.com/2025/day/1
+"""
+
+
+def main() -> None: ...
+
+
+if __name__ == "__main__":
+    main()
---
+++ days/01/test.py
@@ -0,0 +1,4 @@
+"""Day 1: Secret Entrance
+
+https://adventofcode.com/2025/day/1
+"""
