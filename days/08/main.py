af6691a7b8d343ad54dd294afe2a25f55a221241
Martin Grignard <mar.grignard@gmail.com>
2025-12-09 11:06:37 +0100

    feat: :sparkles: Add `compute_distance`

--- days/08/main.py
+++ days/08/main.py
@@ -3,6 +3,7 @@
 https://adventofcode.com/2025/day/8
 """
 
+import sys
 from typing import TypeAlias
 
 
@@ -14,7 +15,18 @@ def parse_position(string: str) -> Position:
     return tuple(int(coordinate) for coordinate in string.split(","))
 
 
-def main() -> None: ...
+def compute_distance(from_position: Position, to_position: Position) -> float:
+    """Compute the Euclidean distance between two points."""
+    return sum(
+        [
+            (to_coord - from_coord) ** 2
+            for from_coord, to_coord in zip(from_position, to_position)
+        ]
+    )
+
+
+def main() -> None:
+    positions = [parse_position(string) for string in sys.stdin]
 
 
 if __name__ == "__main__":
