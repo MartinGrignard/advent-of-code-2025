fd4e2e9aab18ca95bca0221efc365213420f8747
Martin Grignard <mar.grignard@gmail.com>
2025-12-09 08:53:29 +0100

    test: :test_tube: Add `test_split_assessments`

--- days/06/test.py
+++ days/06/test.py
@@ -7,7 +7,7 @@ from io import StringIO
 
 import pytest
 
-from main import execute_problem, parse_problems, Problem
+from main import execute_problem, parse_problems, Problem, split_assessments
 
 
 def test_parse_problems() -> None:
@@ -17,6 +17,21 @@ def test_parse_problems() -> None:
         ("*", (2, 678)),
     ]
     assert parse_problems(strings) == expected_problems
+
+
+def test_split_assessments() -> None:
+    strings = StringIO(
+        "123 328  51 64 \n 45 64  387 23 \n  6 98  215 314\n*   +   *   +  \n"
+    )
+    expected_assessments = [
+        ("123", " 45", "  6", "*  "),
+        ("328", "64 ", "98 ", "+  "),
+        (" 51", "387", "215", "*  "),
+        ("64 ", "23 ", "314", "+  "),
+    ]
+    res = list(split_assessments(strings))
+    print(res)
+    assert res == expected_assessments
 
 
 @pytest.mark.parametrize(
