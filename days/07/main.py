166bb6d41875fe9d832589c2e0d65664426a3072
Martin Grignard <mar.grignard@gmail.com>
2025-12-09 09:30:44 +0100

    feat: :sparkles: Add `parse_diagram`

--- days/07/main.py
+++ days/07/main.py
@@ -3,16 +3,29 @@
 https://adventofcode.com/2025/day/7
 """
 
+from typing import TextIO, TypeAlias
+
+
+Positions: TypeAlias = set[int]
+
+
 EMPTY_SPACE = "."
 
 
-def parse_positions(string: str) -> set[int]:
+def parse_positions(string: str) -> Positions:
     """Extract the positions of non-empty items from a diagram row."""
     return {
         position
         for position, character in enumerate(string)
         if character != EMPTY_SPACE
     }
+
+
+def parse_diagram(strings: TextIO) -> tuple[int, list[Positions]]:
+    """Parse a full diagram."""
+    beam_position = parse_positions(next(strings).strip()).pop()
+    splitter_positions = [parse_positions(string.strip()) for string in strings]
+    return beam_position, splitter_positions
 
 
 def main() -> None: ...
