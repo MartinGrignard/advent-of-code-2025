4782ecf239f9046095a08dc6bf9c03805765295d
Martin Grignard <mar.grignard@gmail.com>
2025-12-09 09:39:10 +0100

    test: :test_tube: Add `test_split_beams`

--- days/07/test.py
+++ days/07/test.py
@@ -6,7 +6,7 @@ https://adventofcode.com/2025/day/7
 from io import StringIO
 import pytest
 
-from main import parse_diagram, parse_positions, Positions
+from main import parse_diagram, parse_positions, Positions, split_beams
 
 
 @pytest.mark.parametrize(
@@ -24,3 +24,19 @@ def test_parse_diagram() -> None:
     strings = StringIO("..S..\n.....\n.^.^.\n")
     expected_diagram = (2, [set(), {1, 3}])
     assert parse_diagram(strings) == expected_diagram
+
+
+@pytest.mark.parametrize(
+    ("beam_positions", "splitter_positions", "expected_positions"),
+    [
+        ({1}, set(), {1}),
+        ({1}, {1}, {0, 2}),
+        ({1, 2}, {1}, {0, 2}),
+    ],
+)
+def test_split_beams(
+    beam_positions: Positions,
+    splitter_positions: Positions,
+    expected_positions: Positions,
+) -> None:
+    assert split_beams(beam_positions, splitter_positions) == expected_positions
