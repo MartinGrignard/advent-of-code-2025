f22dbee164f1a7d6fbeb43abd032440d2ad4a937
Martin Grignard <mar.grignard@gmail.com>
2025-12-09 11:06:25 +0100

    test: :test_tube: Add `test_compute_distance`

--- days/08/test.py
+++ days/08/test.py
@@ -4,7 +4,7 @@ https://adventofcode.com/2025/day/8
 """
 
 import pytest
-from main import parse_position, Position
+from main import compute_distance, parse_position, Position
 
 
 @pytest.mark.parametrize(
@@ -16,3 +16,17 @@ from main import parse_position, Position
 )
 def test_parse_position(string: str, expected_position: Position) -> None:
     assert parse_position(string) == expected_position
+
+
+@pytest.mark.parametrize(
+    ("between_positions", "expected_distance"),
+    [
+        (((0, 0, 0), (1, 0, 0)), 1),
+        (((0, 0, 0), (0, 1, 0)), 1),
+        (((0, 0, 0), (0, 0, 1)), 1),
+    ],
+)
+def test_compute_distance(
+    between_positions: tuple[Position, Position], expected_distance: float
+) -> None:
+    assert compute_distance(*between_positions) == expected_distance
