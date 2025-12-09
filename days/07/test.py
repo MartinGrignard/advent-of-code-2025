178ec060edc0a5ab1eb5eb03f44048b3f0d56541
Martin Grignard <mar.grignard@gmail.com>
2025-12-09 09:30:35 +0100

    test: :test_tube: Add `test_parse_diagram`

--- days/07/test.py
+++ days/07/test.py
@@ -3,9 +3,10 @@
 https://adventofcode.com/2025/day/7
 """
 
+from io import StringIO
 import pytest
 
-from main import parse_positions
+from main import parse_diagram, parse_positions, Positions
 
 
 @pytest.mark.parametrize(
@@ -15,5 +16,11 @@ from main import parse_positions
         (".^..^.", {1, 4}),
     ],
 )
-def test_parse_positions(string: str, expected_positions: set[int]) -> None:
+def test_parse_positions(string: str, expected_positions: Positions) -> None:
     assert parse_positions(string) == expected_positions
+
+
+def test_parse_diagram() -> None:
+    strings = StringIO("..S..\n.....\n.^.^.\n")
+    expected_diagram = (2, [set(), {1, 3}])
+    assert parse_diagram(strings) == expected_diagram
