dc2ee59b311fc7ac7add8a52682ad2b2f5035929
Martin Grignard <mar.grignard@gmail.com>
2025-12-09 11:00:59 +0100

    test: :test_tube: Add `test_parse_position`

--- days/08/test.py
+++ days/08/test.py
@@ -2,3 +2,17 @@
 
 https://adventofcode.com/2025/day/8
 """
+
+import pytest
+from main import parse_position, Position
+
+
+@pytest.mark.parametrize(
+    ("string", "expected_position"),
+    [
+        ("1,2,3", (1, 2, 3)),
+        ("12,34,56", (12, 34, 56)),
+    ],
+)
+def test_parse_position(string: str, expected_position: Position) -> None:
+    assert parse_position(string) == expected_position
