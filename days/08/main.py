ff0cd56f6c49ec19f1ee4c0ad0084a233fae12b2
Martin Grignard <mar.grignard@gmail.com>
2025-12-09 11:01:11 +0100

    feat: :sparkles: Add `parse_position`

--- days/08/main.py
+++ days/08/main.py
@@ -3,6 +3,16 @@
 https://adventofcode.com/2025/day/8
 """
 
+from typing import TypeAlias
+
+
+Position: TypeAlias = tuple[int, int, int]
+
+
+def parse_position(string: str) -> Position:
+    """Parse a single position from a string."""
+    return tuple(int(coordinate) for coordinate in string.split(","))
+
 
 def main() -> None: ...
 
