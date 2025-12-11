81e635ed20761b168330303512adf0134a10ae40
Martin Grignard <mar.grignard@gmail.com>
2025-12-11 15:42:16 +0100

    test: :test_tube: Add `test_compute_pairwise_area`

--- days/09/test.py
+++ days/09/test.py
@@ -5,7 +5,7 @@ https://adventofcode.com/2025/day/9
 
 import pytest
 
-from main import parse_tile, Tile
+from main import compute_pairwise_area, parse_tile, Tile
 
 
 @pytest.mark.parametrize(
@@ -17,3 +17,19 @@ from main import parse_tile, Tile
 )
 def test_parse_tile(string: str, expected_tile: Tile) -> None:
     assert parse_tile(string) == expected_tile
+
+
+def test_compute_pairwise_area() -> None:
+    tiles: list[Tile] = [
+        (0, 0),
+        (1, 0),
+        (0, 1),
+    ]
+    expected_areas = [
+        ((0, 0), (1, 0), 2),
+        ((0, 0), (0, 1), 2),
+        ((1, 0), (0, 1), 4),
+    ]
+    areas = list(compute_pairwise_area(iter(tiles)))
+    print(areas)
+    assert areas == expected_areas
