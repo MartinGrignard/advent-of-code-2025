8c240bf876a8f056506d5e1eea00ce1452f47e1f
Martin Grignard <mar.grignard@gmail.com>
2025-12-11 15:42:58 +0100

    feat: :sparkles: Add `compute_pairwise_area`

--- days/09/main.py
+++ days/09/main.py
@@ -3,7 +3,9 @@
 https://adventofcode.com/2025/day/9
 """
 
-from typing import TypeAlias
+import itertools
+import sys
+from typing import Generator, Iterable, TypeAlias
 
 
 Tile: TypeAlias = tuple[int, int]
@@ -14,7 +16,16 @@ def parse_tile(string: str) -> Tile:
     return tuple(map(int, string.strip().split(",")))
 
 
-def main() -> None: ...
+def compute_pairwise_area(
+    tiles: Iterable[Tile],
+) -> Generator[tuple[Tile, Tile, int], None, None]:
+    """Compute the area of each pair of tiles."""
+    pairs = itertools.combinations(tiles, 2)
+    for from_tile, to_tile in pairs:
+        area = (abs(to_tile[0] - from_tile[0]) + 1) * (
+            abs(to_tile[1] - from_tile[1]) + 1
+        )
+        yield from_tile, to_tile, area
 
 
 if __name__ == "__main__":
