733d38b81a9307c0805476e3fd7ff21d857fee2c
Martin Grignard <mar.grignard@gmail.com>
2025-12-11 16:10:54 +0100

    test: :test_tube: Add `test_create_loop`

--- days/09/test.py
+++ days/09/test.py
@@ -3,9 +3,10 @@
 https://adventofcode.com/2025/day/9
 """
 
+from typing import Iterable
 import pytest
 
-from main import compute_pairwise_area, parse_tile, Tile
+from main import compute_pairwise_area, create_loop, parse_tile, Tile, Loop
 
 
 @pytest.mark.parametrize(
@@ -33,3 +34,14 @@ def test_compute_pairwise_area() -> None:
     areas = list(compute_pairwise_area(iter(tiles)))
     print(areas)
     assert areas == expected_areas
+
+
+@pytest.mark.parametrize(
+    ("tiles", "expected_loop"),
+    [
+        ([(0, 0), (1, 0), (1, 1), (0, 1)], [(0, 0), (1, 0), (1, 1), (0, 1)]),
+        ([(0, 0), (1, 0), (2, 0), (2, 1), (0, 1)], [(0, 0), (2, 0), (2, 1), (0, 1)]),
+    ],
+)
+def test_create_loop(tiles: Iterable[Tile], expected_loop: Loop) -> None:
+    assert create_loop(tiles) == expected_loop
