0064c233add23074453f3571957ed977df2ffee4
Martin Grignard <mar.grignard@gmail.com>
2025-12-11 15:24:04 +0100

    test: :test_tube: Add `test_parse_tile`

--- days/09/test.py
+++ days/09/test.py
@@ -2,3 +2,18 @@
 
 https://adventofcode.com/2025/day/9
 """
+
+import pytest
+
+from main import parse_tile, Tile
+
+
+@pytest.mark.parametrize(
+    ("string", "expected_tile"),
+    [
+        ("7,1", (7, 1)),
+        ("11,1", (11, 1)),
+    ],
+)
+def test_parse_tile(string: str, expected_tile: Tile) -> None:
+    assert parse_tile(string) == expected_tile
