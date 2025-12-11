937d6c1c16982ab9bf39ed2f49677c68c945b530
Martin Grignard <mar.grignard@gmail.com>
2025-12-11 15:24:55 +0100

    feat: :sparkles: Add `parse_tile`

--- days/09/main.py
+++ days/09/main.py
@@ -2,3 +2,20 @@
 
 https://adventofcode.com/2025/day/9
 """
+
+from typing import TypeAlias
+
+
+Tile: TypeAlias = tuple[int, int]
+
+
+def parse_tile(string: str) -> Tile:
+    """Parse a tile."""
+    return tuple(map(int, string.strip().split(",")))
+
+
+def main() -> None: ...
+
+
+if __name__ == "__main__":
+    main()
